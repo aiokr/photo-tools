5f0ea589ad0be5e6e4feaae93dca4afce2b81974
aiokr <hiaiokr@gmail.com>
2023-04-14 11:12:29 +0800

    修改获取时间的方式

--- api/himawari8/index.py
+++ api/himawari8/index.py
@@ -1,30 +1,41 @@
 
 import requests
-import datetime
+import time
 from http.server import BaseHTTPRequestHandler
 
+# 获取当前时间
+ticks = int(time.time())
+print(ticks)
+nowTimeArray = time.gmtime(ticks)
+print(nowTimeArray)
+
+# 将时间变为20分钟前
+bef20 = ticks - 20 * 60
+timeB20Array = time.gmtime(bef20)
+print(timeB20Array)
+
+# 日期格式化成字符串
+date = time.strftime("%Y/%m/%d/", timeB20Array)
+print(date)
+
+# 小时格式化成字符串并补零
+hour = str(time.strftime("%H", timeB20Array)).zfill(2)
+
+# 分钟格式化并补零
+minute = str(time.strftime("%M", timeB20Array))[0] + '0'
+# 设置秒数为 00，将其格式化为字符串
+second = '00'
+
+# 定义卫星图文件的扩展名
+ext = '_0_0.png'
+
+# 拼接出完整的卫星图 URL 地址
+picture_url = 'https://himawari8-dl.nict.go.jp/himawari8/img/D531106/1d/550/' + \
+    date + hour + minute + second + ext
+print(picture_url)
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        # 获取当前时间并将其格式化为字符串
-        date = datetime.datetime.utcnow().strftime('%Y/%m/%d/')
-
-        # 获取当前小时数并减去 1，将其格式化为字符串并补零
-        hour = str(int(datetime.datetime.utcnow().strftime('%H'))).zfill(2)
-
-        # 获取当前分钟数的十位数并补零，将其格式化为字符串
-        minute = str(int(datetime.datetime.utcnow().strftime('%M')) - 20 )[0] + '0'
-        # 设置秒数为 00，将其格式化为字符串
-        second = '00'
-
-        # 定义卫星图文件的扩展名
-        ext = '_0_0.png'
-
-        # 拼接出完整的卫星图 URL 地址
-        picture_url = 'https://himawari8-dl.nict.go.jp/himawari8/img/D531106/1d/550/' + \
-            date + hour + minute + second + ext
-        print(picture_url)
-
         # 发送 HTTP 响应头
         self.send_response(200)
         self.send_header('Content-type', 'image/png')
